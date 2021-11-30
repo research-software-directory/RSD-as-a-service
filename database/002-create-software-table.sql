0f65719a6bd3649880f5b4b2a35db264d1f45c11
Ewan Cahen <e.cahen@esciencecenter.nl>
2021-11-30 12:33:31 +0100

    feat: upgrade to PostgREST 9, Postgres 14 and add timestamp fields and triggers to software table

--- database/002-create-software-table.sql
+++ database/002-create-software-table.sql
@@ -2,5 +2,32 @@ CREATE TABLE software (
 	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
 	slug VARCHAR(100) UNIQUE NOT NULL,
 	brand_name VARCHAR(100) NOT NULL,
-	read_more VARCHAR
+	read_more VARCHAR,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
 );
+
+CREATE FUNCTION sanitise_insert() RETURNS TRIGGER LANGUAGE plpgsql as
+$$
+BEGIN
+	NEW.id = gen_random_uuid();
+	NEW.created_at = LOCALTIMESTAMP;
+	NEW.updated_at = NEW.created_at;
+	return NEW;
+END
+$$;
+
+CREATE TRIGGER sanitise_insert BEFORE INSERT ON software FOR EACH ROW EXECUTE PROCEDURE sanitise_insert();
+
+
+CREATE FUNCTION sanitise_update() RETURNS TRIGGER LANGUAGE plpgsql as
+$$
+BEGIN
+	NEW.id = OLD.id;
+	NEW.created_at = OLD.created_at;
+	NEW.updated_at = LOCALTIMESTAMP;
+	return NEW;
+END
+$$;
+
+CREATE TRIGGER sanitise_update BEFORE UPDATE ON software FOR EACH ROW EXECUTE PROCEDURE sanitise_update();
