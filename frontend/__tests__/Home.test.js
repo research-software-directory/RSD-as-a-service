a323811431389e2449b2e7645565c875439b6a99
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2021-12-10 13:04:39 +0100

    chore: remove unused variable

--- frontend/__tests__/Home.test.js
+++ frontend/__tests__/Home.test.js
@@ -1,5 +1,4 @@
 import {render, screen} from '@testing-library/react'
-import { act } from 'react-dom/test-utils'
 import Home from '../pages/index'
 
 import {WrappedComponentWithProps} from '../utils/jest/WrappedComponents'
