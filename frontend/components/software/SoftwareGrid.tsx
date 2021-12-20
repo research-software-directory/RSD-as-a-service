f5aa2f6c94e050eb80ababa9fedc0c8d266cc1f5
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2021-12-20 18:52:45 +0100

    chore: use tailwind classes instead of css-in-js

--- frontend/components/software/SoftwareGrid.tsx
+++ frontend/components/software/SoftwareGrid.tsx
@@ -1,5 +1,4 @@
 import ContentInTheMiddle from "../layout/ContentInTheMiddle"
-import CardGrid from '../layout/CardGrid'
 import SoftwareCard from './SoftwareCard'
 import {SoftwareItem} from '../../types/SoftwareItem'
 
@@ -16,19 +15,19 @@ export default function SoftwareGrid({software}:{software:SoftwareItem[]}){
   }
 
   return (
-    <CardGrid>
-    {software.map(item=>{
-      return(
-        <SoftwareCard
-          key={`/software/${item.slug}/`}
-          href={`/software/${item.slug}/`}
-          brand_name={item.brand_name}
-          short_statement={item.short_statement}
-          is_featured={item.is_featured}
-          updated_at={item.updated_at}
-        />
-      )
-    })}
-    </CardGrid>
+    <section className='grid grid-cols-1 gap-[0.125rem] sm:grid-cols-2 lg:grid-cols-3 hd:grid-cols-4 py-4 px=[0.0625]'>
+      {software.map(item=>{
+        return(
+          <SoftwareCard
+            key={`/software/${item.slug}/`}
+            href={`/software/${item.slug}/`}
+            brand_name={item.brand_name}
+            short_statement={item.short_statement}
+            is_featured={item.is_featured}
+            updated_at={item.updated_at}
+          />
+        )
+      })}
+    </section>
   )
 }
