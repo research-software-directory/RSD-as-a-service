1d17aeb13e202e647a8bc2f9bc8aa9b12d78e2bf
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2021-12-20 23:00:10 +0100

    chore: bump version to 0.2.1

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     // Run ESLint in these directories during production builds (next build)
+    // by default next runs linter only in pages/, components/, and lib/
     dirs: ["components","config","pages","styles","types","utils"]
   },
 }
