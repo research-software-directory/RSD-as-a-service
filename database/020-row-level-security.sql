94f693f20d6ab38454eaf84befd93b0a88d20e90
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-01-03 19:05:15 +0100

    feat: policies for all tables related to software for maintainers

--- database/020-row-level-security.sql
+++ database/020-row-level-security.sql
@@ -31,6 +31,10 @@ ALTER TABLE repository_url ENABLE ROW LEVEL SECURITY;
 CREATE POLICY anyone_can_read ON repository_url FOR SELECT TO web_anon
 	USING (software IN (SELECT id FROM software));
 
+CREATE POLICY maintainer_all_rights ON repository_url TO rsd_user
+	USING (software IN (SELECT id FROM software))
+	WITH CHECK (software IN (SELECT id FROM software));
+
 CREATE POLICY admin_all_rights ON repository_url TO rsd_admin
 	USING (TRUE)
 	WITH CHECK (TRUE);
@@ -40,6 +44,10 @@ ALTER TABLE license_for_software ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY anyone_can_read ON license_for_software FOR SELECT TO web_anon
 	USING (software IN (SELECT id FROM software));
+
+CREATE POLICY maintainer_all_rights ON license_for_software TO rsd_user
+	USING (software IN (SELECT id FROM software))
+	WITH CHECK (software IN (SELECT id FROM software));
 
 CREATE POLICY admin_all_rights ON license_for_software TO rsd_admin
 	USING (TRUE)
@@ -51,6 +59,10 @@ ALTER TABLE contributor ENABLE ROW LEVEL SECURITY;
 CREATE POLICY anyone_can_read ON contributor FOR SELECT TO web_anon
 	USING (software IN (SELECT id FROM software));
 
+CREATE POLICY maintainer_all_rights ON contributor TO rsd_user
+	USING (software IN (SELECT id FROM software))
+	WITH CHECK (software IN (SELECT id FROM software));
+
 CREATE POLICY admin_all_rights ON contributor TO rsd_admin
 	USING (TRUE)
 	WITH CHECK (TRUE);
@@ -60,6 +72,10 @@ ALTER TABLE software_for_software ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY anyone_can_read ON software_for_software FOR SELECT TO web_anon
 	USING (origin IN (SELECT id FROM software) AND relation IN (SELECT id FROM software));
+
+CREATE POLICY maintainer_all_rights ON software_for_software TO rsd_user
+	USING (origin IN (SELECT id FROM software) AND relation IN (SELECT id FROM software))
+	WITH CHECK (origin IN (SELECT id FROM software) AND relation IN (SELECT id FROM software));
 
 CREATE POLICY admin_all_rights ON software_for_software TO rsd_admin
 	USING (TRUE)
@@ -71,6 +87,10 @@ ALTER TABLE tag_for_software ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY anyone_can_read ON tag_for_software FOR SELECT TO web_anon
 	USING (software IN (SELECT id FROM software));
+
+CREATE POLICY maintainer_all_rights ON tag_for_software TO rsd_user
+	USING (software IN (SELECT id FROM software))
+	WITH CHECK (software IN (SELECT id FROM software));
 
 CREATE POLICY admin_all_rights ON tag_for_software TO rsd_admin
 	USING (TRUE)
