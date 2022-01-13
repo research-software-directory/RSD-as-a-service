1a675aefe68f6c3faa36603c6c16aa1c79da050d
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-01-13 19:37:47 +0100

    feat: add status for inter relationships

--- database/012-inter-relation-tables.sql
+++ database/012-inter-relation-tables.sql
@@ -1,6 +1,13 @@
+CREATE TYPE relation_status as ENUM (
+	'requested_by_origin',
+	'requested_by_relation',
+	'approved'
+);
+
 CREATE TABLE software_for_software (
 	origin UUID references software (id),
 	relation UUID references software (id),
+	status relation_status NOT NULL DEFAULT 'approved',
 	PRIMARY KEY (origin, relation)
 );
 
@@ -8,6 +15,7 @@ CREATE TABLE software_for_software (
 CREATE TABLE software_for_project (
 	software UUID references software (id),
 	project UUID references project (id),
+	status relation_status NOT NULL DEFAULT 'approved',
 	PRIMARY KEY (software, project)
 );
 
@@ -15,6 +23,7 @@ CREATE TABLE software_for_project (
 CREATE TABLE project_for_project (
 	origin UUID references project (id),
 	relation UUID references project (id),
+	status relation_status NOT NULL DEFAULT 'approved',
 	PRIMARY KEY (origin, relation)
 );
 
@@ -22,6 +31,7 @@ CREATE TABLE project_for_project (
 CREATE TABLE software_for_organisation (
 	software UUID references software (id),
 	organisation UUID references organisation (id),
+	status relation_status NOT NULL DEFAULT 'approved',
 	PRIMARY KEY (software, organisation)
 );
 
@@ -29,5 +39,6 @@ CREATE TABLE software_for_organisation (
 CREATE TABLE project_for_organisation (
 	project UUID references project (id),
 	organisation UUID references organisation (id),
+	status relation_status NOT NULL DEFAULT 'approved',
 	PRIMARY KEY (project, organisation)
 );
