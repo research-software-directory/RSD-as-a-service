0b504e16c978d45668e040026b09a072784323ba
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-01-19 20:08:51 +0100

    wip: remove uploading test coverage content and add alinea tp home page to test PR trigger after pushing additional code to PR.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home(){
       <DefaultLayout>
         <h1>Home page</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, eligendi, magnam eaque quas necessitatibus in eius suscipit numquam dolorem cumque nulla recusandae debitis, deserunt ipsam distinctio quaerat illum. Provident, quos.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, eligendi, magnam eaque quas necessitatibus in eius suscipit numquam dolorem cumque nulla recusandae debitis, deserunt ipsam distinctio quaerat illum. Provident, quos.</p>
       </DefaultLayout>
     </>
   )
