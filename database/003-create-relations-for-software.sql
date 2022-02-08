95fa807994a0606cc0e0bd261f693f6bd465c3f6
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-02-08 16:44:04 +0100

    refactor: only allow for one repo url per software entry

--- database/003-create-relations-for-software.sql
+++ database/003-create-relations-for-software.sql
@@ -1,6 +1,5 @@
 CREATE TABLE repository_url (
-	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-	software UUID references software (id) NOT NULL,
+	software UUID references software (id) PRIMARY KEY,
 	url VARCHAR NOT NULL,
 	languages JSONB,
 	languages_scraped_at TIMESTAMP,
@@ -9,28 +8,6 @@ CREATE TABLE repository_url (
 	commit_history JSONB,
 	commit_history_scraped_at TIMESTAMP
 );
-
-
-CREATE FUNCTION sanitise_insert_repository_url() RETURNS TRIGGER LANGUAGE plpgsql as
-$$
-BEGIN
-	NEW.id = gen_random_uuid();
-	return NEW;
-END
-$$;
-
-CREATE TRIGGER sanitise_insert_repository_url BEFORE INSERT ON software FOR EACH ROW EXECUTE PROCEDURE sanitise_insert_repository_url();
-
-
-CREATE FUNCTION sanitise_update_repository_url() RETURNS TRIGGER LANGUAGE plpgsql as
-$$
-BEGIN
-	NEW.id = OLD.id;
-	return NEW;
-END
-$$;
-
-CREATE TRIGGER sanitise_update_repository_url BEFORE UPDATE ON software FOR EACH ROW EXECUTE PROCEDURE sanitise_update_repository_url();
 
 
 
