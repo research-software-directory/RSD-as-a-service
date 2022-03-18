218497d6ddd331b71290db53326589bf34ef85fe
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-03-18 12:07:54 +0100

    refactor: change logo table structure and dependencies

--- database/011-create-organisation-table.sql
+++ database/011-create-organisation-table.sql
@@ -1,5 +1,43 @@
+CREATE TABLE organisation (
+	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	slug VARCHAR(100) UNIQUE,
+	primary_maintainer UUID REFERENCES account (id),
+	name VARCHAR UNIQUE NOT NULL,
+	ror_id VARCHAR UNIQUE,
+	website VARCHAR UNIQUE NOT NULL,
+	is_tenant BOOLEAN DEFAULT FALSE NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL
+);
+
+CREATE FUNCTION sanitise_insert_organisation() RETURNS TRIGGER LANGUAGE plpgsql as
+$$
+BEGIN
+	NEW.id = gen_random_uuid();
+	NEW.created_at = LOCALTIMESTAMP;
+	NEW.updated_at = NEW.created_at;
+	return NEW;
+END
+$$;
+
+CREATE TRIGGER sanitise_insert_organisation BEFORE INSERT ON organisation FOR EACH ROW EXECUTE PROCEDURE sanitise_insert_organisation();
+
+
+CREATE FUNCTION sanitise_update_organisation() RETURNS TRIGGER LANGUAGE plpgsql as
+$$
+BEGIN
+	NEW.id = OLD.id;
+	NEW.slug = OLD.slug;
+	NEW.created_at = OLD.created_at;
+	NEW.updated_at = LOCALTIMESTAMP;
+	return NEW;
+END
+$$;
+
+CREATE TRIGGER sanitise_update_organisation BEFORE UPDATE ON organisation FOR EACH ROW EXECUTE PROCEDURE sanitise_update_organisation();
+
 CREATE TABLE logos (
-	id VARCHAR(64) PRIMARY KEY,
+	id UUID references organisation(id) PRIMARY KEY,
 	data VARCHAR NOT NULL,
 	mime_type VARCHAR(100) NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -34,43 +72,3 @@ BEGIN
 	END IF;
 END
 $$;
-
-
-CREATE TABLE organisation (
-	id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-	slug VARCHAR(100) UNIQUE,
-	primary_maintainer UUID REFERENCES account (id),
-	name VARCHAR UNIQUE NOT NULL,
-	ror_id VARCHAR UNIQUE,
-	logo_id VARCHAR(64) references logos(id),
-	website VARCHAR UNIQUE NOT NULL,
-	is_tenant BOOLEAN DEFAULT FALSE NOT NULL,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL
-);
-
-CREATE FUNCTION sanitise_insert_organisation() RETURNS TRIGGER LANGUAGE plpgsql as
-$$
-BEGIN
-	NEW.id = gen_random_uuid();
-	NEW.created_at = LOCALTIMESTAMP;
-	NEW.updated_at = NEW.created_at;
-	return NEW;
-END
-$$;
-
-CREATE TRIGGER sanitise_insert_organisation BEFORE INSERT ON organisation FOR EACH ROW EXECUTE PROCEDURE sanitise_insert_organisation();
-
-
-CREATE FUNCTION sanitise_update_organisation() RETURNS TRIGGER LANGUAGE plpgsql as
-$$
-BEGIN
-	NEW.id = OLD.id;
-	NEW.slug = OLD.slug;
-	NEW.created_at = OLD.created_at;
-	NEW.updated_at = LOCALTIMESTAMP;
-	return NEW;
-END
-$$;
-
-CREATE TRIGGER sanitise_update_organisation BEFORE UPDATE ON organisation FOR EACH ROW EXECUTE PROCEDURE sanitise_update_organisation();
