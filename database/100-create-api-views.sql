cd573a41f9a6b99ecf15b4a421312254af13196c
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-03-22 20:59:24 +0100

    feat: create views for organisation edit section and overview

--- database/100-create-api-views.sql
+++ database/100-create-api-views.sql
@@ -62,3 +62,47 @@ FROM
 ORDER BY
 	display_name asc
 ;
+
+-- Participating organisations by software
+CREATE VIEW organisations_for_software AS
+SELECT
+	organisation.id as id,
+	organisation.slug,
+	organisation.primary_maintainer,
+	organisation.name,
+	organisation.ror_id,
+	organisation.is_tenant,
+	organisation.website,
+	logo_for_organisation.id as logo_id,
+	software_for_organisation.status,
+	software.id as software
+FROM
+	software
+INNER JOIN
+	software_for_organisation on software.id=software
+INNER JOIN
+	organisation on software_for_organisation.organisation = organisation.id
+LEFT JOIN
+	logo_for_organisation on logo_for_organisation.id = organisation.id
+;
+
+-- Software count by organisation
+CREATE VIEW software_count_by_organisation AS
+SELECT
+	organisation, count(organisation) as software_cnt
+FROM
+	software_for_organisation
+GROUP BY organisation
+;
+
+-- Organisations overview
+CREATE VIEW organisations_overview AS
+SELECT
+	organisation.id as id,slug,name,website,ror_id,logo_for_organisation.id as logo_id,software_cnt
+FROM
+	organisation
+LEFT JOIN
+	software_count_by_organisation on organisation = organisation.id
+LEFT JOIN
+	logo_for_organisation on organisation = logo_for_organisation.id
+;
