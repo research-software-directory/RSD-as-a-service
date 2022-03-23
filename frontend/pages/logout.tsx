044cbff678c0fbdfecf67df189d7a7c77c7fc42b
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-03-23 16:38:48 +0100

    feat: when logout redirect to home

--- frontend/pages/logout.tsx
+++ frontend/pages/logout.tsx
@@ -1,45 +1,57 @@
 import {useEffect} from 'react'
+import {GetServerSidePropsContext} from 'next'
+import {useRouter} from 'next/router'
 import ContentInTheMiddle from '../components/layout/ContentInTheMiddle'
 import {getRsdTokenNode, removeRsdTokenNode} from '../auth'
 import Button from '@mui/material/Button'
-import {useAuth, defaultSession, Session} from '../auth'
 
-export default function Logout({session}:{session:Session}) {
-  const {setSession} = useAuth()
+type RedirectProps = {
+  destination: string,
+  statusCode?: number,
+  permanent: boolean
+}
+
+export default function Logout({redirect}: { redirect: RedirectProps }) {
+  const router = useRouter()
+  // in case we did reach frontend page somehow?!?
+  // we will redirect you (again)
   useEffect(() => {
-    setSession(session)
-  },[session,setSession])
+    router.replace(redirect.destination)
+  },[redirect.destination,router])
+
+  // this content should not be visible
   return (
     <ContentInTheMiddle>
       <div className="border p-12">
         <h1>You are logged out!</h1>
         <p className="py-8">
-          You can go now doing something else :-)
-         </p>
-         <Button onClick={() => window.location.href = '/'}>
+          You can now do something else :-)
+        </p>
+        <Button onClick={() => window.location.href = '/'}>
            Home
-         </Button>
+        </Button>
       </div>
     </ContentInTheMiddle>
   )
 }
 
-export function getServerSideProps(context: any) {
-  const {req, res} = context
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  const {req,res} = context
   // get token
   const token = getRsdTokenNode(req)
-  // console.group('Logout')
-  // console.log('session...', session)
+  // get last visited path
+  // const redirect = req.cookies['rsd_pathname']
+  // console.log('redirect to...', redirect)
   // remove old cookie
   if (token) {
     // console.log('removeRsdTokenNode...')
     removeRsdTokenNode(res)
   }
-  // console.groupEnd()
+  // redirect to home
   return {
-    props: {
-      // return default
-      session: defaultSession
-    }
+    redirect: {
+      destination: '/',
+      permanent: false
+    },
   }
 }
