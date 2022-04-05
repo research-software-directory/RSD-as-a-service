174cab0eda4c798bb813097d8ae27ff15bc50894
Jesús García <ctw@ctwhome.com>
2022-04-05 14:59:05 +0200

    minor: move login button to its own component

---
+++ frontend/components/login/LoginButton.tsx
@@ -0,0 +1,38 @@
+import {useAuth} from '~/auth'
+import {userMenuItems} from '~/config/userMenuItems'
+import {getRedirectUrl} from '~/utils/surfConext'
+import UserMenu from '~/components/layout/UserMenu'
+
+
+export default function LoginButton() {
+
+  const {session} = useAuth()
+  const status = session?.status || 'loading'
+
+  // todo: provisional redirect to Surf directly
+  async function redirectToSurf() {
+    const url = await getRedirectUrl('surfconext')
+    if (url) {
+      window.location.href = url
+    }
+  }
+
+  if (status === 'loading') {
+    return null
+  }
+  if (status === 'authenticated') {
+    //   // when user authenticated
+    //   // we show user menu with the avatar and user specific options
+    return (
+      <UserMenu
+        name='No Name'
+        menuOptions={userMenuItems}
+      />
+    )
+  } else {
+    return (
+      <a onClick={redirectToSurf}>
+        Sign in
+      </a>)
+  }
+}
