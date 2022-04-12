b4ca46f03e4d0f13d865114ea6a69fd9b8349614
Christian Meeßen <14222414+cmeessen@users.noreply.github.com>
2022-04-12 13:11:06 +0200

    feat: Add scrapeGitLab() to MainCommits

--- scrapers/src/main/java/nl/esciencecenter/rsd/scraper/MainCommits.java
+++ scrapers/src/main/java/nl/esciencecenter/rsd/scraper/MainCommits.java
@@ -1,5 +1,7 @@
 package nl.esciencecenter.rsd.scraper;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,7 +12,46 @@ public class MainCommits {
 	public static void main(String[] args) {
 		System.out.println("Start scraping commits");
 		scrapeGitHub();
+		scrapeGitLab();
 		System.out.println("Done scraping commits");
+	}
+
+	private static void scrapeGitLab() {
+		SoftwareInfoRepository existingCommitsSorted = new OrderByDateSIRDecorator(new PostgrestSIR(Config.backendBaseUrl(), codePlatformProvider.github));
+		Collection<RepositoryUrlData> dataToScrape = existingCommitsSorted.commitData();
+		Collection<RepositoryUrlData> updatedDataAll = new ArrayList<>();
+		LocalDateTime scrapedAt = LocalDateTime.now();
+		int countRequests = 0;
+		int maxRequests = Config.maxRequestsGithub();
+		for (RepositoryUrlData commitData : dataToScrape) {
+			try {
+				countRequests += 1;
+				if (countRequests > maxRequests) break;
+				String repoUrl = commitData.url();
+				String hostname = "";
+				try {
+					hostname = new URI(repoUrl).getHost();
+				} catch (URISyntaxException e) {
+					System.out.println("Error obtaining hostname of repository with url: " + repoUrl);
+					e.printStackTrace();
+				}
+				String apiUrl = "https://" + hostname + "/api";
+				String projectPath = repoUrl.replace("https://" + hostname + "/", "");
+				if (projectPath.endsWith("/")) projectPath = projectPath.substring(0, projectPath.length() - 1);
+
+				String scrapedCommits = new AggregateContributionsPerWeekSIDecorator(new GitLabSI(apiUrl, projectPath)).contributionsGitLab();
+				RepositoryUrlData updatedData = new RepositoryUrlData(
+						commitData.software(), commitData.url(), "github",
+						commitData.license(), commitData.licenseScrapedAt(),
+						scrapedCommits, scrapedAt,
+						commitData.languages(), commitData.languagesScrapedAt());
+				updatedDataAll.add(updatedData);
+			} catch (RuntimeException e) {
+				System.out.println("Exception when handling data from url " + commitData.url() + ":");
+				e.printStackTrace();
+			}
+		}
+		new PostgrestSIR(Config.backendBaseUrl() + "/repository_url", codePlatformProvider.github).save(updatedDataAll);
 	}
 
 	private static void scrapeGitHub() {
