e1ed4a68c403623f357f918563b4cab69b8ce066
Christian Meeßen <14222414+cmeessen@users.noreply.github.com>
2022-05-26 20:09:07 +0200

    feat: add link to data protection declaration

--- frontend/components/layout/AppFooter.tsx
+++ frontend/components/layout/AppFooter.tsx
@@ -72,6 +72,12 @@ export default function AppFooter () {
               rel="noreferrer">
               Technical Documentation
             </a>
+            <a href="https://www.gfz-potsdam.de/en/data-protection/"
+              target="_blank"
+              className="footer-link"
+              rel="noreferrer">
+              Data protection declaration
+            </a>
           </div>
         </div>
       </div>
