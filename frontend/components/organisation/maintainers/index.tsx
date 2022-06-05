a44649b1b6f21bca0beb6b790df8c5d5e946ad6b
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-06-05 14:23:30 +0200

    feat: invite for organisation maintainers

--- frontend/components/organisation/maintainers/index.tsx
+++ frontend/components/organisation/maintainers/index.tsx
@@ -1,8 +1,130 @@
+import {useEffect,useState} from 'react'
 
-export default function OrganisationMaintainers() {
+import {Session} from '~/auth'
+import ContentLoader from '~/components/layout/ContentLoader'
+import EditSection from '~/components/layout/EditSection'
+import EditSectionTitle from '~/components/layout/EditSectionTitle'
+import {maintainers as config} from '~/components/projects/edit/maintainers/config'
+import ConfirmDeleteModal from '~/components/layout/ConfirmDeleteModal'
+import useSnackbar from '~/components/snackbar/useSnackbar'
+import useOrganisationMaintainers, {
+  deleteMaintainerFromOrganisation, MaintainerOfOrganisation
+} from './useOrganisationMaintainer'
+import OrganisationMaintainerLink from './OrganisationMaintainerLink'
+import {OrganisationForOverview} from '~/types/Organisation'
+import OrganisationMaintainersList from './OrganisationMaintainersList'
+
+type DeleteModal = {
+  open: boolean,
+  pos?: number,
+  displayName?:string
+}
+
+
+export default function OrganisationMaintainers({session, organisation}:
+  {session: Session, organisation:OrganisationForOverview }) {
+  const {showErrorMessage} = useSnackbar()
+  const {loading,maintainers} = useOrganisationMaintainers({
+    organisation: organisation.id ?? '',
+    token: session.token
+  })
+  const [organisationMaintainers, setOrganisationMaintaners] = useState<MaintainerOfOrganisation[]>([])
+  const [modal, setModal] = useState<DeleteModal>({
+    open: false
+  })
+
+  useEffect(() => {
+    let abort = false
+    if (loading === false &&
+      abort === false) {
+      setOrganisationMaintaners(maintainers)
+    }
+    return () => { abort = true }
+  },[maintainers,loading])
+
+  if (loading) {
+    return (
+      <ContentLoader />
+    )
+  }
+
+  function closeModal() {
+    setModal({
+      open: false
+    })
+  }
+
+  function onDeleteMaintainer(pos: number) {
+    const maintainer = maintainers[pos]
+    if (maintainer) {
+      setModal({
+        open: true,
+        pos,
+        displayName: maintainer.name
+      })
+    }
+  }
+
+  async function deleteMaintainer(pos: number) {
+    // console.log('delete maintainer...pos...', pos)
+    closeModal()
+    const admin = maintainers[pos]
+    if (admin) {
+      const resp = await deleteMaintainerFromOrganisation({
+        maintainer: admin.account,
+        organisation: organisation.id ?? '',
+        token: session.token,
+        frontend: true
+      })
+      if (resp.status === 200) {
+        const newMaintainersList = [
+          ...maintainers.slice(0, pos),
+          ...maintainers.slice(pos+1)
+        ]
+        setOrganisationMaintaners(newMaintainersList)
+      } else {
+        showErrorMessage(`Failed to remove maintainer. ${resp.message}`)
+      }
+    }
+  }
+
   return (
-    <section>
-      <h2>Under construction</h2>
-    </section>
+    <>
+      <EditSection className='xl:grid xl:grid-cols-[1fr,1fr] xl:px-0 xl:gap-[3rem]'>
+        <div className="py-4 xl:pl-[3rem]">
+          <EditSectionTitle
+            title={config.title}
+          />
+          <OrganisationMaintainersList
+            onDelete={onDeleteMaintainer}
+            maintainers={organisationMaintainers}
+          />
+        </div>
+        <div className="py-4 min-w-[21rem] xl:my-0">
+          <EditSectionTitle
+            title={config.inviteLink.title}
+            subtitle={config.inviteLink.subtitle}
+          />
+          <OrganisationMaintainerLink
+            organisation={organisation.id ?? ''}
+            account={session.user?.account ?? ''}
+            token={session.token}
+          />
+        </div>
+      </EditSection>
+      <ConfirmDeleteModal
+        open={modal.open}
+        title="Remove maintainer"
+        body={
+          <p>Are you sure you want to remove <strong>{modal.displayName ?? 'No name'}</strong>?</p>
+        }
+        onCancel={() => {
+          setModal({
+            open:false
+          })
+        }}
+        onDelete={()=>deleteMaintainer(modal.pos ?? 0)}
+      />
+    </>
   )
 }
---
+++ frontend/pages/invite/organisation/[id].tsx
@@ -0,0 +1,108 @@
+import {GetServerSidePropsContext} from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import {claimOrganisationMaintainerInvite} from '~/auth/api/authHelpers'
+import {getAccountFromToken} from '~/auth/jwtUtils'
+import ContentInTheMiddle from '~/components/layout/ContentInTheMiddle'
+import DefaultLayout from '~/components/layout/DefaultLayout'
+import PageErrorMessage from '~/components/layout/PageErrorMessage'
+import PageTitle from '~/components/layout/PageTitle'
+
+type InviteOrganisationMaintainerProps = {
+  organisationInfo: {
+    slug: string,
+    name: string,
+  }|null,
+  error: {
+    status: number,
+    message: string
+  }|null
+}
+
+export default function InviteOrganisationMaintainer({organisationInfo, error}:
+  InviteOrganisationMaintainerProps) {
+  // console.group('InviteOrganisationMaintainer')
+  // console.log('organisationInfo..', organisationInfo)
+  // console.log('error..', error)
+  // console.groupEnd()
+  function renderContent() {
+    if (error!==null) {
+      return (
+        <PageErrorMessage {...error}/>
+      )
+    }
+    return (
+      <ContentInTheMiddle>
+        <h2>
+          You are now a maintainer of {organisationInfo?.name ?? 'missing'}!
+          &nbsp;
+          <Link href={`/organisations/${organisationInfo?.slug ?? 'missing'}`}>
+            <a>
+              Open organisation page
+            </a>
+          </Link>
+        </h2>
+      </ContentInTheMiddle>
+    )
+  }
+
+  return (
+     <DefaultLayout>
+      <Head>
+        <title>Organisation Maintainer Invite | RSD</title>
+      </Head>
+      <PageTitle title="Organisation Maintainer Invite" />
+      {renderContent()}
+    </DefaultLayout>
+  )
+}
+
+// fetching data server side
+// see documentation https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // extract from page-query
+  const {params,req} = context
+  // extract rsd_token
+  const token = req?.cookies['rsd_token']
+  // extract id
+  const id = params?.id
+
+  // extract account id from token
+  const account = getAccountFromToken(token)
+  // console.log('account...', account)
+  if (typeof account == 'undefined') {
+    return {
+      props: {
+        projectInfo: null,
+        error: {
+          status: 401,
+          message: 'You need to sign in to RSD first!'
+        }
+      }
+    }
+  }
+
+  if (id) {
+    // claim the software maintainer invite
+    const resp:InviteOrganisationMaintainerProps = await claimOrganisationMaintainerInvite({
+      id: id.toString(),
+      token,
+      frontend: false
+    })
+    // pass software info to page component as props
+    return {
+      props: resp
+    }
+  } else {
+    return {
+      props: {
+        softwareInfo:null,
+        error: {
+          status: 404,
+          message: 'This invite is invalid. It\'s missing invite id. Please ask the organisation mantainer to provide you a new link.'
+        }
+      }
+    }
+  }
+}
