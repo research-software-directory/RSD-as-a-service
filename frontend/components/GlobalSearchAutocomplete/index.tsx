1fa6c31b3c75d91bbd1a060bade8c8a97159724b
Jesús García <ctw@ctwhome.com>
2022-06-14 22:21:16 +0200

    add key and stayles to floating box

--- frontend/components/GlobalSearchAutocomplete/index.tsx
+++ frontend/components/GlobalSearchAutocomplete/index.tsx
@@ -22,23 +22,23 @@ const data = [
 ]
 export default function GlobalSearchAutocomplete() {
   return (
-  <div className="z-10 border px-3 py-2 flex relative ml-auto rounded-sm">
-    <svg className="absolute right-[10px] top-[10px]" width="22" viewBox="0 0 18 18" fill="none"
-         xmlns="http://www.w3.org/2000/svg">
-      <circle cx="8.98438" cy="9.10718" r="8.22705" stroke="currentColor"/>
-    </svg>
-    <input type="search" className="bg-transparent focus:outline-none"
-           placeholder="Search or jump to" autoComplete="off"/>
-    <div className="absolute top-[60px] w-full bg-white text-black py-2">
-      {[0, 1, 2, 3].map(item =>
-        <div className="flex gap-2 hover:bg-blue-500 p-2">
-          <svg className="" width="22" viewBox="0 0 18 18" fill="none"
-               xmlns="http://www.w3.org/2000/svg">
-            <circle cx="8.98438" cy="9.10718" r="8.22705" stroke="currentColor"/>
-          </svg>
-          hello</div>)
-      }
+    <div className="z-10 border px-3 py-2 flex relative ml-auto rounded-sm">
+      <svg className="absolute right-[10px] top-[10px]" width="22" viewBox="0 0 18 18" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+        <circle cx="8.98438" cy="9.10718" r="8.22705" stroke="currentColor"/>
+      </svg>
+      <input type="search" className="bg-transparent focus:outline-none"
+             placeholder="Search or jump to" autoComplete="off"/>
+      <div className="shadow-xl absolute top-[60px] w-full bg-white text-black py-2">
+        {[0, 1, 2, 3].map(item =>
+          <div key={item} className="flex gap-2 hover:bg-[#09A1E3] p-2 cursor-pointer transition">
+            <svg className="" width="22" viewBox="0 0 18 18" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+              <circle cx="8.98438" cy="9.10718" r="8.22705" stroke="currentColor"/>
+            </svg>
+            hello</div>)
+        }
+      </div>
     </div>
-  </div>
   )
 }
