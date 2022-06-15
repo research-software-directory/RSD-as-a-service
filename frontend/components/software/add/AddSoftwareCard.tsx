8e852e7caa39ae50061d13bafe5e5c484e85d0cb
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-06-15 12:43:53 +0200

    fix: remove duplicated SPDX entry

--- frontend/components/software/add/AddSoftwareCard.tsx
+++ frontend/components/software/add/AddSoftwareCard.tsx
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2022 Christian Meeßen (GFZ) <christian.meessen@gfz-potsdam.de>
-// SPDX-FileCopyrightText: 2022 Dusan Mijatovic
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
 // SPDX-FileCopyrightText: 2022 Matthias Rüster (GFZ) <matthias.ruester@gfz-potsdam.de>
--- frontend/components/software/edit/information/SoftwareLicenses.tsx
+++ frontend/components/software/edit/information/SoftwareLicenses.tsx
@@ -1,4 +1,3 @@
-// SPDX-FileCopyrightText: 2022 Dusan Mijatovic
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
 // SPDX-FileCopyrightText: 2022 Matthias Rüster (GFZ) <matthias.ruester@gfz-potsdam.de>
