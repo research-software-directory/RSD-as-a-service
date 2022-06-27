b1990ed45595ccce207a30a8239f870b119b09c3
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-06-27 16:01:21 +0200

    style: replace spaces with tabs for indentation

--- database/100-create-api-views.sql
+++ database/100-create-api-views.sql
@@ -1056,14 +1056,14 @@ $$;
 -- software_cnt, project_cnt, organisation_cnt
 -- this rpc returns json object instead of array
 CREATE FUNCTION homepage_counts(
-    OUT software_cnt BIGINT,
-    OUT project_cnt BIGINT,
-    OUT organisation_cnt BIGINT
+	OUT software_cnt BIGINT,
+	OUT project_cnt BIGINT,
+	OUT organisation_cnt BIGINT
 ) LANGUAGE plpgsql STABLE AS
 $$
 BEGIN
-    SELECT count(id) FROM software INTO software_cnt;
-    SELECT count(id) FROM project INTO project_cnt;
-    SELECT count(id) FROM organisation WHERE parent IS NULL INTO organisation_cnt;
+	SELECT count(id) FROM software INTO software_cnt;
+	SELECT count(id) FROM project INTO project_cnt;
+	SELECT count(id) FROM organisation WHERE parent IS NULL INTO organisation_cnt;
 END
 $$;
