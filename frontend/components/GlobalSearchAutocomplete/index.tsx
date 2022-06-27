3bf6dcdf591f177c6f606d0434a43b30b4f9f00d
Jesús García <ctw@ctwhome.com>
2022-06-27 14:01:01 +0200

    fix: negation in conditional for unpublished label

--- frontend/components/GlobalSearchAutocomplete/index.tsx
+++ frontend/components/GlobalSearchAutocomplete/index.tsx
@@ -148,7 +148,7 @@ export default function GlobalSearchAutocomplete(props: Props) {
                     <div className="font-normal line-clamp-1">{item?.name}</div>
 
                     <div className="text-xs text-current text-opacity-40">
-                      {item?.source}{!item?.is_published === false && <span className="flex-nowrap border px-1 py-[2px] rounded bg-warning ml-3 text-xs text-white">unpublished</span>}
+                      {item?.source}{item?.is_published === false && <span className="flex-nowrap border px-1 py-[2px] rounded bg-warning ml-3 text-xs text-white">unpublished</span>}
                     </div>
 
                   </div>
