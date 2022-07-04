1f73c1bf0c6d163e33ca25f295666ab8604856a0
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-07-04 19:26:16 +0200

    chore: remove double section tags

--- frontend/components/software/TestimonialsSection.tsx
+++ frontend/components/software/TestimonialsSection.tsx
@@ -12,19 +12,17 @@ export default function TestimonialSection({testimonials = []}: { testimonials: 
   if (testimonials?.length === 0) return null
 
   return (
-    <section>
-      <PageContainer className="py-12 px-4 lg:grid lg:grid-cols-[1fr,4fr]">
-        <h2
-          data-testid="software-contributors-section-title"
-          className="pb-8 text-[2rem] text-primary">
-          Testimonials
-        </h2>
-        <section>
-          {testimonials.map((item, pos) => {
-            return (<TestimonialItem key={pos} item={item} />)
-          })}
-        </section>
-      </PageContainer>
-    </section>
+    <PageContainer className="py-12 px-4 lg:grid lg:grid-cols-[1fr,4fr]">
+      <h2
+        data-testid="software-contributors-section-title"
+        className="pb-8 text-[2rem] text-primary">
+        Testimonials
+      </h2>
+      <section>
+        {testimonials.map((item, pos) => {
+          return (<TestimonialItem key={pos} item={item} />)
+        })}
+      </section>
+    </PageContainer>
   )
 }
