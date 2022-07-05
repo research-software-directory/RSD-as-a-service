6e01304750d84ae119728445e3af98729ad8508b
Dusan Mijatovic (xps2018) <d.mijatovic@dv4all.nl>
2022-07-05 16:54:05 +0200

    chore: add under counstruction gif

--- frontend/pages/privacy.tsx
+++ frontend/pages/privacy.tsx
@@ -8,6 +8,14 @@ export default function PrivacyPolicyPage(){
       <main className="flex-1 bg-secondary">
         <article className="flex flex-col flex-1 px-4 overflow-hidden lg:container lg:mx-auto">
           <h1 className="py-8 text-white">Privacy policy</h1>
+          <div className='flex h-[10rem] my-8'>
+            <img
+              src="https://maggiebrennan.com/press/img/underconstruction.gif"
+              alt="under construction"
+              height="160px"
+              width="160px"
+            />
+          </div>
         </article>
       </main>
       <AppFooter/>
