0f53375d04dc8509e42d290520a6decb8f1e6012
Norman Ziegner <norman.ziegner@ufz.de>
2022-07-21 14:50:59 +0200

    fix: correct typo in 'Login failed' message
    Signed-off-by: Norman Ziegner <norman.ziegner@ufz.de>

--- frontend/pages/login/failed.tsx
+++ frontend/pages/login/failed.tsx
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2022 Helmholtz Centre for Environmental Research (UFZ)
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -12,7 +13,7 @@ export default function LoginFailed() {
       <div className="border p-12">
         <h1>Login failed</h1>
         <p className="py-8">
-          Unfortunatelly, something went wrong during the login process
+          Unfortunately, something went wrong during the login process.
         </p>
         <Link href="/" passHref>
           <a>Go home</a>
