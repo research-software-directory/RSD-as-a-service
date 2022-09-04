a080adad5a1d53be3a589a29cd4b584ccf8acec7
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-09-04 21:47:38 +0200

    fix: add no items message to tesimonials list

--- frontend/components/software/edit/testimonials/SortableTestimonialList.tsx
+++ frontend/components/software/edit/testimonials/SortableTestimonialList.tsx
@@ -10,6 +10,8 @@ import EditIcon from '@mui/icons-material/Edit'
 import {Testimonial} from '~/types/Testimonial'
 import SortableList from '~/components/layout/SortableList'
 import SortableTestimonialListItem from './SortableTestimonialListItem'
+import Alert from '@mui/material/Alert'
+import AlertTitle from '@mui/material/AlertTitle'
 
 type SortableTestimonialProps={
   items: Testimonial[],
@@ -18,7 +20,15 @@ type SortableTestimonialProps={
   onSorted:(items: Testimonial[])=>void
 }
 
-export default function SortableTestimonialList({items, onEdit, onDelete, onSorted}:SortableTestimonialProps){
+export default function SortableTestimonialList({items, onEdit, onDelete, onSorted}: SortableTestimonialProps) {
+  if (items.length === 0) {
+    return (
+      <Alert severity="warning" sx={{marginTop:'0.5rem'}}>
+        <AlertTitle sx={{fontWeight:500}}>No testimonials</AlertTitle>
+        Add one using <strong>ADD button!</strong>
+      </Alert>
+    )
+  }
   /**
    * This method is called by SortableList component to enable
    * rendering of custom sortable items
