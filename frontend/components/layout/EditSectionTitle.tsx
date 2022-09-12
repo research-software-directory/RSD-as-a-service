06dc978363c4d2d6c8f3ab5b7124b82a2b4b3a8b
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-09-12 15:50:49 +0200

    fix: add info link to project edit page in sidebar for research domains

--- frontend/components/layout/EditSectionTitle.tsx
+++ frontend/components/layout/EditSectionTitle.tsx
@@ -1,13 +1,18 @@
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
+// SPDX-FileCopyrightText: 2022 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
 // SPDX-FileCopyrightText: 2022 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
 // SPDX-FileCopyrightText: 2022 Matthias Rüster (GFZ) <matthias.ruester@gfz-potsdam.de>
+// SPDX-FileCopyrightText: 2022 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2022 dv4all
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import Link from '@mui/material/Link'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+
 export default function EditSectionTitle(
-  {title, subtitle = '', children, hlevel = 2}:
-  {title: string, subtitle?: string, children?: any, hlevel?: number}
+  {title, subtitle = '', children, hlevel = 2, infoLink}:
+  {title: string, subtitle?: string, children?: any, hlevel?: number, infoLink?: string}
 ) {
 
   const HeadingTag: any = `h${hlevel}`
@@ -26,7 +31,7 @@ export default function EditSectionTitle(
     return (
       <>
         <div className="flex">
-          <HeadingTag className="flex-1">{title}</HeadingTag>
+          <HeadingTag className="flex-1">{title} {infoLink && <Link href={infoLink} target="_blank" rel="noreferrer"><InfoOutlinedIcon fontSize="small"/></Link>}</HeadingTag>
           {children}
         </div>
         {getSubtitle()}
@@ -36,7 +41,7 @@ export default function EditSectionTitle(
 
   return (
     <>
-      <HeadingTag>{title}</HeadingTag>
+      <HeadingTag>{title} {infoLink && <Link href={infoLink} target="_blank" rel="noreferrer"><InfoOutlinedIcon fontSize="small"/></Link>}</HeadingTag>
       {getSubtitle()}
     </>
   )
