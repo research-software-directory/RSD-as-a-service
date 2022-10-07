dd9b638ee9087e412def193c0c830845b63fc0fa
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-10-07 20:04:32 +0200

    feat: orcid whitelist page ux

--- frontend/pages/admin/orcid-whitelist.tsx
+++ frontend/pages/admin/orcid-whitelist.tsx
@@ -4,7 +4,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import Head from 'next/head'
-import {GetServerSidePropsContext} from 'next'
 
 import {app} from '../../config/app'
 import RsdAdminContent from '~/auth/RsdAdminContent'
@@ -13,17 +12,18 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import DeleteIcon from '@mui/icons-material/Delete'
 import IconButton from '@mui/material/IconButton'
-import {createJsonHeaders} from '~/utils/fetchHelpers'
+import {createJsonHeaders, extractReturnMessage} from '~/utils/fetchHelpers'
 import useSnackbar from '~/components/snackbar/useSnackbar'
 import EditSectionTitle from '~/components/layout/EditSectionTitle'
 import {useSession} from '~/auth'
 import {useEffect, useState} from 'react'
 import Link from '@mui/material/Link'
 import {Button, TextField} from '@mui/material'
+import {PageTitleSticky} from '~/components/layout/PageTitle'
 
 export default function OrcidWitelistPage() {
   const pageTitle = `ORCID whitelist | ${app.title}`
-  const {showErrorMessage, showInfoMessage} = useSnackbar()
+  const {showErrorMessage} = useSnackbar()
 
   const [orcids, setOrcids] = useState<string[]>([])
   const {token} = useSession()
@@ -48,8 +48,10 @@ export default function OrcidWitelistPage() {
       headers: createJsonHeaders(token),
       method: 'POST'
     })
-    if (resp.status !== 201) showErrorMessage('Failed to add ORCID.')
 
+    const err = await extractReturnMessage(resp)
+
+    if (err.status !== 200) showErrorMessage(`Failed to add ORCID. ${err.message} `)
     fetchWhitelistedOrcids()
   }
 
@@ -59,22 +61,55 @@ export default function OrcidWitelistPage() {
         <title>{pageTitle}</title>
       </Head>
       <RsdAdminContent>
-        <EditSectionTitle title={'Add an ORCID'}/>
-        <form onSubmit={(e) => {e.preventDefault(); submitOrcid()}}>
-          <TextField id="orcid-input" required placeholder="0000-0000-0000-0000" inputProps={{pattern: '^\\d{4}-\\d{4}-\\d{4}-\\d{3}[0-9X]$'}}/>
-          <Button variant="contained" type='submit'>Add</Button>
-        </form>
-        <OrcidWhitelist
-          orcids={orcids}
-          onDeleteCallback={() => fetchWhitelistedOrcids()}
-          token={token}/>
+        <PageTitleSticky
+          style={{padding:'1rem 0rem 2rem 0rem'}}
+        >
+          <div className="w-full flex justify-between items-center">
+            <h1 className="flex-1 w-full md:mt-4">ORCID whitelist</h1>
+          </div>
+        </PageTitleSticky>
+        <section className="flex flex-col-reverse gap-8 md:grid md:grid-cols-2">
+          <div>
+            <EditSectionTitle
+              title={'Allowed ORCIDs'}
+            >
+              <h2 className="pr-2">{orcids.length}</h2>
+            </EditSectionTitle>
+            <div className='py-2'></div>
+            <OrcidWhitelist
+              orcids={orcids}
+              onDeleteCallback={() => fetchWhitelistedOrcids()}
+              token={token} />
+          </div>
+          <div>
+            <EditSectionTitle title={'Add an ORCID'} />
+            <div className='py-2'></div>
+            <form onSubmit={(e) => {
+              e.preventDefault()
+              submitOrcid()
+            }}>
+              <TextField
+                id="orcid-input"
+                variant="standard"
+                autoComplete='off'
+                placeholder="0000-0000-0000-0000"
+                helperText="Provide valid orcid"
+                inputProps={{pattern: '^\\d{4}-\\d{4}-\\d{4}-\\d{3}[0-9X]$'}}
+                required
+              />
+              <Button variant="text" type='submit'>Add to whitelist</Button>
+            </form>
+          </div>
+        </section>
+        <div className='py-8'></div>
       </RsdAdminContent>
     </DefaultLayout>
   )
 }
 
-function OrcidWhitelist({orcids, onDeleteCallback, token}: {orcids: string[], onDeleteCallback: Function, token: string}) {
-  const {showErrorMessage, showInfoMessage} = useSnackbar()
+function OrcidWhitelist({orcids, onDeleteCallback, token}:
+  { orcids: string[], onDeleteCallback: Function, token: string }) {
+  const {showErrorMessage} = useSnackbar()
 
   async function deleteOrcid(orcid: string) {
     const resp = await fetch(`/api/v1/orcid_whitelist?orcid=eq.${orcid}`, {
@@ -85,22 +120,22 @@ function OrcidWhitelist({orcids, onDeleteCallback, token}: {orcids: string[], on
     onDeleteCallback()
   }
 
-
-  return(
-    <>
-      <EditSectionTitle title={'Whitelisted ORCIDs'}/>
-      <List>
-        {orcids.map(orcid => {
-          return (
-            <ListItem key={orcid} disableGutters>
-              <Link href={`https://orcid.org/${orcid}`} underline="always" target="_blank" rel="noreferrer">
-                {orcid}
-              </Link>
+  return (
+    <List>
+      {orcids.map(orcid => {
+        return (
+          <ListItem
+            key={orcid} disableGutters
+            secondaryAction={
               <IconButton onClick={() => deleteOrcid(orcid)}><DeleteIcon/></IconButton>
-            </ListItem>
-          )
-        })}
-      </List>
-    </>
+            }
+          >
+            <Link href={`https://orcid.org/${orcid}`} underline="always" target="_blank" rel="noreferrer">
+              {orcid}
+            </Link>
+          </ListItem>
+        )
+      })}
+    </List>
   )
 }
