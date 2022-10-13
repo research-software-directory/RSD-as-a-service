d1a5a1c3661328eac46b67bbd5c1ed8190352b25
Ewan Cahen <e.cahen@esciencecenter.nl>
2022-10-13 15:14:34 +0200

    fix: only save mention once if DataCite returns a result multiple times

--- scrapers/src/main/java/nl/esciencecenter/rsd/scraper/doi/DataciteMentionRepository.java
+++ scrapers/src/main/java/nl/esciencecenter/rsd/scraper/doi/DataciteMentionRepository.java
@@ -17,7 +17,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class DataciteMentionRepository implements MentionRepository {
@@ -86,13 +88,20 @@ public class DataciteMentionRepository implements MentionRepository {
 				.collect(Collectors.joining("\",\"", "\"", "\""));
 	}
 
-	static Collection<MentionRecord> jsonStringToMention(String json) {
+	static Collection<MentionRecord> jsonStringToUniqueMentions(String json) {
 		JsonObject root = JsonParser.parseString(json).getAsJsonObject();
 		JsonArray worksJson = root.getAsJsonObject("data").getAsJsonObject("works").getAsJsonArray("nodes");
 		Collection<MentionRecord> mentions = new ArrayList<>();
+		Set<String> usedDois = new HashSet<>();
 		for (JsonElement work : worksJson) {
 			try {
-				mentions.add(parseWork(work.getAsJsonObject()));
+//				Sometimes, DataCite gives back two of the same results for one DOI, e.g. for 10.4122/1.1000000817,
+//				so we need to only add it once, otherwise we cannot POST it to the backend
+				MentionRecord parsedMention = parseWork(work.getAsJsonObject());
+				if (usedDois.contains(parsedMention.doi)) continue;
+
+				usedDois.add(parsedMention.doi);
+				mentions.add(parsedMention);
 			} catch (RuntimeException e) {
 				System.out.println("Failed to scrape a DataCite mention with data " + work);
 				e.printStackTrace();
@@ -154,7 +163,7 @@ public class DataciteMentionRepository implements MentionRepository {
 		JsonObject body = new JsonObject();
 		body.addProperty("query", QUERY_UNFORMATTED.formatted(joinCollection(dois)));
 		String responseJson = Utils.post("https://api.datacite.org/graphql", body.toString(), "Content-Type", "application/json");
-		return jsonStringToMention(responseJson);
+		return jsonStringToUniqueMentions(responseJson);
 	}
 
 	@Override
