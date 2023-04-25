42c532bb0aba8b247fc258dc8abc2dba619d9149
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-04-25 18:55:54 +0200

    feat: software filters on highlights page adapt to already made filtering and selections feat: all api requests on highlights page are done server side refactor: split filter markup into separate components for future use

---
+++ frontend/components/software/overview/filters/FilterHeader.tsx
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2023 Dusan Mijatovic (dv4all)
+// SPDX-FileCopyrightText: 2023 dv4all
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import Button from '@mui/material/Button'
+
+type FilterHeaderProps = {
+  filterCnt: number
+  resetFilters: () => void
+}
+
+export default function FilterHeader({filterCnt,resetFilters}:FilterHeaderProps) {
+  return (
+    <div className="flex justify-between">
+      <div className="flex justify-center items-center gap-2 mr-12">
+        <span
+          className="rounded-full bg-gray-100 h-8 w-8 flex items-center justify-center font-semibold">
+          {filterCnt}
+        </span>
+        Filters
+      </div>
+
+      <Button
+        size="small"
+        onClick={resetFilters}
+        disabled={filterCnt === 0}
+        variant="outlined"
+        color="primary"
+      >
+        Clear
+      </Button>
+    </div>
+  )
+}
