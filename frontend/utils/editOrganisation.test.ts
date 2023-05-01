f07efc12301d5a3603c0ffb5e1bad3eb382c0aa4
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-05-01 11:48:21 +0200

    fix: missing organisation slug on maintainer invite confirmation page

---
+++ frontend/utils/editOrganisation.test.ts
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2023 Dusan Mijatovic (dv4all)
+// SPDX-FileCopyrightText: 2023 dv4all
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import {mockResolvedValueOnce} from './jest/mockFetch'
+import {getRsdPathForOrganisation} from './editOrganisation'
+
+describe('getRsdPathForOrganisation', () => {
+  it('returns rsd_path in response message', async () => {
+    const mockData = {
+      organisation: 'TEST-ORGANISATION-NAME',
+      rsd_path: 'test_organisation_path',
+      parent_names: 'TEST-PARENT-NAMES'
+    }
+    // mock response
+    mockResolvedValueOnce(mockData)
+    // get organisation path to link to in the message
+    const slug = await getRsdPathForOrganisation({
+      uuid: 'TEST_ORGANISATION_UUID',
+      token: 'TEST_TOKEN'
+    })
+
+    expect(slug.status).toEqual(200)
+    expect(slug.message).toEqual(mockData.rsd_path)
+  })
+
+  it('returns custom error message from api', async () => {
+    const mockError = {
+      status: 400,
+      message: 'This is custom error message from api'
+    }
+    // mock error response
+    mockResolvedValueOnce({
+      message: mockError.message
+    }, {
+      status: mockError.status,
+      statusText: 'Not OK'
+    })
+    // get organisation path to link to in the message
+    const slug = await getRsdPathForOrganisation({
+      uuid: 'TEST_ORGANISATION_UUID',
+      token: 'TEST_TOKEN'
+    })
+
+    expect(slug.status).toEqual(mockError.status)
+    expect(slug.message).toEqual(mockError.message)
+  })
+})
+
