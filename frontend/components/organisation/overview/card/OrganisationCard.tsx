98439d3597a8c7aebc5577782e81b736613be12d
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-08-15 19:42:03 +0200

    feat: add verified organisation badge and claim organisation button

--- frontend/components/organisation/overview/card/OrganisationCard.tsx
+++ frontend/components/organisation/overview/card/OrganisationCard.tsx
@@ -14,7 +14,7 @@ import ImageWithPlaceholder from '~/components/layout/ImageWithPlaceholder'
 import CardImageFrame from '~/components/cards/CardImageFrame'
 import CardContentFrame from '~/components/cards/CardContentFrame'
 import CountryLabel from './CountryLabel'
-import TenantStatus from './TenantStatus'
+import TenantBadge from './TenantBadge'
 
 export type OrganisationCardProps = {
   id: string,
@@ -31,40 +31,59 @@ export type OrganisationCardProps = {
 export default function OrganisationCard({organisation}: { organisation: OrganisationCardProps }) {
 
   return (
-    <Link
-      data-testid="organisation-card-link"
-      href={`/organisations/${organisation.rsd_path}?tab=software&order=mention_cnt`}
-      className="flex h-full hover:text-inherit"
-      passHref
-    >
-      {/* Organization card content */}
-      <div className="flex flex-col transition overflow-hidden bg-base-100 shadow-md hover:shadow-lg rounded-lg hover:cursor-pointer select-none w-full relative">
-        <CardImageFrame>
-          <ImageWithPlaceholder
-            src={`${getImageUrl(organisation.logo_id) ?? ''}`}
-            alt={`Logo for ${organisation.name}`}
-            type="gradient"
-            className={`w-full text-base-content-disabled ${organisation.logo_id ? 'p-4':''}`}
-            bgSize={'contain'}
+    <div className="relative">
+      <Link
+        data-testid="organisation-card-link"
+        href={`/organisations/${organisation.rsd_path}?tab=software&order=mention_cnt`}
+        className="flex h-full hover:text-inherit"
+        passHref
+      >
+        <div className = "flex flex-col transition overflow-hidden bg-base-100 shadow-md hover:shadow-lg rounded-lg hover:cursor-pointer select-none w-full relative" >
+          <CardImageFrame>
+            <ImageWithPlaceholder
+              src={`${getImageUrl(organisation.logo_id) ?? ''}`}
+              alt={`Logo for ${organisation.name}`}
+              type="gradient"
+              className={`w-full text-base-content-disabled ${organisation.logo_id ? 'p-4':''}`}
+              bgSize={'contain'}
+            />
+          </CardImageFrame>
+          <CardContentFrame>
+            <div className="flex-1">
+              <CountryLabel country={organisation.country} />
+              <CardTitleSubtitle
+                title={organisation.name}
+                subtitle={organisation.short_description ?? ''}
+              />
+            </div>
+            <div className="flex gap-8 justify-evenly text-center">
+              <OrganisationCardMetrics
+                software_cnt={organisation.software_cnt}
+                project_cnt={organisation.project_cnt}
+              />
+              {/* if is not tenant we render the placeholder here and TenantBadge outside the Link */}
+              {organisation.is_tenant === false ?
+                <div className="min-w-[3rem]" />
+                :
+                <TenantBadge
+                  is_tenant={organisation.is_tenant}
+                  organisation={organisation.name}
+                />
+              }
+            </div>
+          </CardContentFrame>
+        </div>
+      </Link>
+      {/* if is not the tenant TenantBadge needs to be outside organisation link for email link to work */}
+      {organisation.is_tenant === false ?
+        <div style={{position: 'absolute',right:'2.5rem', bottom: '1rem'}}>
+          <TenantBadge
+            is_tenant={organisation.is_tenant}
+            organisation={organisation.name}
           />
-        </CardImageFrame>
-        <TenantStatus is_tenant={organisation.is_tenant} />
-        <CardContentFrame>
-          <div className="flex-1">
-            <CountryLabel country={organisation.country} />
-            <CardTitleSubtitle
-              title={organisation.name}
-              subtitle={organisation.short_description ?? ''}
-            />
-          </div>
-          <div className="flex gap-8 justify-evenly text-center">
-            <OrganisationCardMetrics
-              software_cnt={organisation.software_cnt}
-              project_cnt={organisation.project_cnt}
-            />
-          </div>
-        </CardContentFrame>
-      </div>
-    </Link>
+        </div>
+        : null
+      }
+    </div>
   )
 }
