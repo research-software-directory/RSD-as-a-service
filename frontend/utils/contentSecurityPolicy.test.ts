8ab3e149f9eff3a5165321a75cbac345a32e07fe
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-09-04 23:07:45 +0200

    lighthouse: change csp values adviced during audit

--- frontend/utils/contentSecurityPolicy.test.ts
+++ frontend/utils/contentSecurityPolicy.test.ts
@@ -1,5 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all) (dv4all)
 // SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2023 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -18,8 +20,7 @@ beforeEach(() => {
 it('sets content security policy header for development', () => {
   const nonce = setContentSecurityPolicyHeader(res as any)
   const policyName = 'Content-Security-Policy-Report-Only'
-  const policyText = `default-src \'self\'; style-src \'self\' \'unsafe-inline\'; connect-src \'self\' https://*; font-src \'self\' https://fonts.gstatic.com; img-src \'self\' data: https://*; script-src \'self\'  \'unsafe-eval\' \'nonce-${nonce}'`
-
+  const policyText = `default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*; base-uri 'none'; object-src 'none'; script-src 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval' 'unsafe-inline'`
 
   expect(mockSetHeader).toBeCalledTimes(1)
   // const calledWith = mockSetHeader
@@ -31,7 +32,10 @@ it('sets content security policy header for production', () => {
   process.env.MATOMO_URL = 'https://mamtomo.com/test-url'
   const nonce = setContentSecurityPolicyHeader(res as any)
   const policyName = 'Content-Security-Policy'
-  const policyText = `default-src \'self\'; style-src \'self\' \'unsafe-inline\'; connect-src \'self\' https://*; font-src \'self\' https://fonts.gstatic.com; img-src \'self\' data: https://*; script-src \'self\' https://mamtomo.com/test-url  \'nonce-${nonce}'`
+  const policyText = `default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*; base-uri 'none'; object-src 'none'; script-src 'nonce-${nonce}' 'strict-dynamic' https://mamtomo.com/test-url 'unsafe-inline'`
+
+
+  // "default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*; base-uri 'none'; object-src 'none'; script-src 'self' 'unsafe-inline' https://mamtomo.com/test-url 'nonce-b771ce36-a563-4e69-b969-0a758ac0762e'"
 
 
   expect(mockSetHeader).toBeCalledTimes(1)
