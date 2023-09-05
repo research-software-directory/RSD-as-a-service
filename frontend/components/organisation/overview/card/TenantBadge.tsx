fb60134d00a65b77874dfd99c6e93fe3f721eba7
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-09-05 16:55:29 +0200

    refactor: only show organisation member badge, do not offer claim organisation option

--- frontend/components/organisation/overview/card/TenantBadge.tsx
+++ frontend/components/organisation/overview/card/TenantBadge.tsx
@@ -3,69 +3,21 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import {useState} from 'react'
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
-import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
-import UnpublishedIcon from '@mui/icons-material/Unpublished'
 
-import OrganisationSignUpDialog from '~/components/home/rsd/OrganisationSignUpDialog'
-
-type TenantBadgeProps = {
-  is_tenant: boolean
-  organisation: string
-}
-
-export default function TenantBadge({is_tenant,organisation}: TenantBadgeProps) {
-  const [modal,setModal]=useState(false)
-
-  // console.group('TenantBadge')
-  // console.log('is_tenant...', is_tenant)
-  // console.log('modal...', modal)
-  // console.log('organisation...', organisation)
-  // console.groupEnd()
-
-  if (is_tenant === true) {
-    return (
-      <div className="flex items-end text-base-600">
-        <Tooltip
-          title="Verified organisation"
-          placement='top'
-          sx={{
-            cursor:'default'
-          }}
-        >
-          <CheckCircleOutlineIcon sx={{width:'1.5rem', height:'1.5rem',margin:'0.75rem'}} />
-        </Tooltip>
-      </div>
-    )
-  }
-
+export default function TenantBadge() {
   return (
-    <div className="flex items-end">
-      <Tooltip describeChild
-        title="Click here to contact us and claim this organisation."
+    <div className="flex items-end text-base-600">
+      <Tooltip
+        title="Member organisation"
         placement='top'
-        // arrow
+        sx={{
+          cursor:'default'
+        }}
       >
-        <IconButton
-          size='large'
-          onClick={(e) => {
-            setModal(true)
-            e.preventDefault()
-          }}>
-          <UnpublishedIcon sx={{width:'1.5rem', height:'1.5rem'}} />
-        </IconButton>
+        <CheckCircleOutlineIcon sx={{width:'1.5rem', height:'1.5rem',margin:'0rem 0.25rem'}} />
       </Tooltip>
-      {modal &&
-        <OrganisationSignUpDialog
-          title="Claim an organisation"
-          reason={`I would like to become an administrator of ${organisation}.`}
-          onClose={() => setModal(false)}
-          open={modal}
-          initOrg={organisation }
-        />
-      }
     </div>
   )
 }
