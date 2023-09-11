2e3767aa10f7642f11a72dbe32284e328d8324e5
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-09-11 14:36:52 +0200

    chore: move duplicated code into MentionEditButton (sonar audit)

---
+++ frontend/components/mention/MentionEditButtons.tsx
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2023 Netherlands eScience Center
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import {useSession} from '~/auth'
+import useEditMentionReducer from './useEditMentionReducer'
+
+import IconButton from '@mui/material/IconButton'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import {MentionItemProps} from '~/types/Mention'
+
+export default function MentionEditButtons({item}: {item:MentionItemProps}) {
+  const {user} = useSession()
+  const {setEditModal,confirmDelete} = useEditMentionReducer()
+  const html = []
+
+  // console.group('MentionEditButtons')
+  // console.log('user...', user)
+  // console.log('item...', item)
+  // console.groupEnd()
+
+  if (user?.role==='rsd_admin') {
+    // items without DOI can be edited by rsd_admin
+    html.push(
+      <IconButton
+        data-testid="edit-mention-btn"
+        key="edit-button"
+        onClick={()=>setEditModal(item)}>
+        <EditIcon />
+      </IconButton>
+    )
+  }
+  // all items can be deleted
+  html.push(
+    <IconButton
+      data-testid="delete-mention-btn"
+      key="delete-button"
+      sx={{
+        marginLeft:'1rem'
+      }}
+      onClick={() => confirmDelete(item)}>
+      <DeleteIcon />
+    </IconButton>
+  )
+
+  return html
+
+}
--- frontend/components/mention/MentionEditItem.tsx
+++ frontend/components/mention/MentionEditItem.tsx
@@ -6,14 +6,9 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import IconButton from '@mui/material/IconButton'
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-// import UpdateIcon from '@mui/icons-material/Update'
-import {useSession} from '~/auth'
 import MentionItemBase from './MentionItemBase'
 import {MentionItemProps} from '~/types/Mention'
-import useEditMentionReducer from './useEditMentionReducer'
+import MentionEditButtons from './MentionEditButtons'
 
 type MentionListItem = {
   pos: number
@@ -21,50 +16,13 @@ type MentionListItem = {
 }
 
 export default function MentionEditItem({item, pos}: MentionListItem) {
-  const {user} = useSession()
-  // use context methods to pass btn action
-  // const {onUpdate, confirmDelete, setEditModal} = useContext(EditMentionContext)
-  const {setEditModal,confirmDelete} = useEditMentionReducer()
-
-  function onEdit() {
-    setEditModal(item)
-  }
-
-  function renderButtons() {
-    const html = []
-
-    if (user?.role==='rsd_admin') {
-      // items without DOI can be edited by rsd_admin
-      html.push(
-        <IconButton
-          data-testid="edit-mention-btn"
-          key="edit-button"
-          onClick={onEdit}>
-          <EditIcon />
-        </IconButton>
-      )
-    }
-    // all items can be deleted
-    html.push(
-      <IconButton
-        data-testid="delete-mention-btn"
-        key="delete-button"
-        sx={{
-          marginLeft:'1rem'
-        }}
-        onClick={() => confirmDelete(item)}>
-        <DeleteIcon />
-      </IconButton>
-    )
-
-    return html
-  }
-
   return (
     <MentionItemBase
       item={item}
       pos={pos}
-      nav={renderButtons()}
+      nav={
+        <MentionEditButtons item={item}/>
+      }
       role="list"
     />
   )
