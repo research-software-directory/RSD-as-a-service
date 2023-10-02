c47e6b7aafda8f1a10c52da07d97dbec116b98f6
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-10-02 16:30:57 +0200

    fix: redirect to ordered software overview on login

--- frontend/auth/locationCookie.ts
+++ frontend/auth/locationCookie.ts
@@ -1,7 +1,8 @@
+// SPDX-FileCopyrightText: 2022 - 2023 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2022 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -21,7 +22,7 @@ export function saveLocationCookie() {
       break
     case '/':
       // root is send to /software
-      document.cookie = `rsd_pathname=${location.href}software;path=/auth;SameSite=None;Secure`
+      document.cookie = `rsd_pathname=${location.href}software?order=mention_cnt;path=/auth;SameSite=None;Secure`
       break
     default:
       // write simple browser cookie
