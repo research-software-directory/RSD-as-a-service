d1a79b8f9754943575dee2d9aabfa4f0c0480a87
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2023-10-03 17:00:57 +0200

    feat: add reference papers page to edit software section

--- database/109-mention-views.sql
+++ database/109-mention-views.sql
@@ -1,3 +1,4 @@
+-- SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
 -- SPDX-FileCopyrightText: 2023 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
 -- SPDX-FileCopyrightText: 2023 Netherlands eScience Center
 --
@@ -20,4 +21,62 @@ $$
 		SELECT mention FROM reference_paper_for_software
 	)
 	GROUP BY mention.id
+$$;
+
+-- UNIQUE CITATIONS BY SOFTWARE ID
+CREATE FUNCTION citation_by_software() RETURNS TABLE (
+	software UUID,
+	id UUID,
+	doi CITEXT,
+	url VARCHAR,
+	title VARCHAR,
+	authors VARCHAR,
+	publisher VARCHAR,
+	publication_year SMALLINT,
+	journal VARCHAR,
+	page VARCHAR,
+	image_url VARCHAR,
+	mention_type mention_type,
+	source VARCHAR,
+	reference_papers UUID[]
+)LANGUAGE sql STABLE AS
+$$
+SELECT
+	reference_paper_for_software.software,
+	mention.id,
+	mention.doi,
+	mention.url,
+	mention.title,
+	mention.authors,
+	mention.publisher,
+	mention.publication_year,
+	mention.journal,
+	mention.page,
+	mention.image_url,
+	mention.mention_type,
+	mention.source,
+	ARRAY_AGG(
+		reference_paper_for_software.mention
+	) AS reference_paper
+FROM
+	reference_paper_for_software
+INNER JOIN
+	citation_for_mention ON citation_for_mention.mention = reference_paper_for_software.mention
+INNER JOIN
+	mention ON mention.id = citation_for_mention.citation
+GROUP BY
+	reference_paper_for_software.software,
+	mention.id,
+	mention.doi,
+	mention.url,
+	mention.title,
+	mention.authors,
+	mention.publisher,
+	mention.publication_year,
+	mention.journal,
+	mention.page,
+	mention.image_url,
+	mention.mention_type,
+	mention.source
+;
 $$;
