3126bb2f4958a5f0c53a015a46a8245cb162a785
Ewan Cahen <e.cahen@esciencecenter.nl>
2024-01-24 12:26:25 +0100

    fix: change database function return type to allow inlining

--- database/118-backend-logs-views.sql
+++ database/118-backend-logs-views.sql
@@ -1,6 +1,6 @@
+-- SPDX-FileCopyrightText: 2023 - 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+-- SPDX-FileCopyrightText: 2023 - 2024 Netherlands eScience Center
 -- SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
--- SPDX-FileCopyrightText: 2023 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
--- SPDX-FileCopyrightText: 2023 Netherlands eScience Center
 --
 -- SPDX-License-Identifier: Apache-2.0
 
@@ -8,27 +8,25 @@ CREATE FUNCTION slug_from_log_reference(
 	table_name VARCHAR,
 	reference_id UUID
 )
-RETURNS TABLE (
-	slug VARCHAR
-)
+RETURNS VARCHAR
 LANGUAGE sql STABLE AS
 $$
 SELECT CASE
 	WHEN table_name = 'repository_url' THEN (
 		SELECT
-			CONCAT('/software/', slug, '/edit/information') as slug
+			CONCAT('/software/', slug, '/edit/information')
 		FROM
 			software WHERE id = reference_id
 	)
 	WHEN table_name = 'package_manager' THEN (
 		SELECT
-			CONCAT('/software/', slug, '/edit/package-managers') as slug
+			CONCAT('/software/', slug, '/edit/package-managers')
 		FROM
 			software
 		WHERE id = (SELECT software FROM package_manager WHERE id = reference_id))
 	WHEN table_name = 'mention' AND reference_id IS NOT NULL THEN (
 		SELECT
-			CONCAT('/api/v1/mention?id=eq.', reference_id) as slug
+			CONCAT('/api/v1/mention?id=eq.', reference_id)
 	)
 	END
 $$;
