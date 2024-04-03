64cb3b9ecc4fcf85c14c8f16f05eb863aa525b18
Ewan Cahen <e.cahen@esciencecenter.nl>
2024-04-03 12:34:30 +0200

    fix: don't show terms and conditions modal to rsd admins anymore

--- frontend/components/user/settings/UserAgreementModal.tsx
+++ frontend/components/user/settings/UserAgreementModal.tsx
@@ -1,10 +1,11 @@
 // SPDX-FileCopyrightText: 2022 - 2023 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 - 2023 Dusan Mijatovic (dv4all) (dv4all)
 // SPDX-FileCopyrightText: 2022 - 2023 dv4all
+// SPDX-FileCopyrightText: 2023 - 2024 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2023 Christian Meeßen (GFZ) <christian.meessen@gfz-potsdam.de>
 // SPDX-FileCopyrightText: 2023 Dusan Mijatovic (Netherlands eScience Center)
 // SPDX-FileCopyrightText: 2023 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
-// SPDX-FileCopyrightText: 2023 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -66,7 +67,7 @@ export default function UserAgrementModal() {
   */
   useEffect(() => {
     let abort = false
-    if (loading === false &&
+    if (loading === false && user?.role !== 'rsd_admin' &&
       (agree_terms === false || notice_privacy_statement === false)
     ) {
       if (abort===false) setOpen(true)
