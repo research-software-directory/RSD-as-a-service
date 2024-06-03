294b12c9696b980c41795f5e6f2bc9c9d1663850
Ewan Cahen <e.cahen@esciencecenter.nl>
2024-06-03 15:54:21 +0200

    featp: add expiration date for maintainer invitations

--- frontend/utils/getUnusedInvitations.ts
+++ frontend/utils/getUnusedInvitations.ts
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2022 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2022 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2022 - 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2022 - 2024 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,7 +7,7 @@ import {Invitation} from '~/types/Invitation'
 import {createJsonHeaders} from './fetchHelpers'
 
 export async function getUnusedInvitations(type: 'software' | 'project' | 'organisation', id: string, token?: string) {
-  const resp = await fetch(`/api/v1/invite_maintainer_for_${type}?select=id,created_at&order=created_at&${type}=eq.${id}&claimed_by=is.null&claimed_at=is.null`, {
+  const resp = await fetch(`/api/v1/invite_maintainer_for_${type}?select=id,created_at,expires_at&order=created_at&${type}=eq.${id}&claimed_by=is.null&claimed_at=is.null`, {
     headers: createJsonHeaders(token)
   })
   const respJson: Invitation[] = await resp.json()
