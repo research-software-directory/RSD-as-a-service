c5f9c351cbae44821063979fef22066a482f6e04
Ewan Cahen <e.cahen@esciencecenter.nl>
2024-08-13 14:33:52 +0200

    refactor: extract duplicate methods and test them

--- authentication/src/main/java/nl/esciencecenter/rsd/authentication/Utils.java
+++ authentication/src/main/java/nl/esciencecenter/rsd/authentication/Utils.java
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2022 - 2023 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2022 - 2023 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2022 - 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2022 - 2024 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -14,8 +14,13 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.StringJoiner;
 
 public class Utils {
+
+	private Utils() {
+	}
 
 	public static URI getTokenUrlFromWellKnownUrl(URI wellKnownUrl) throws IOException, InterruptedException {
 		HttpRequest request = HttpRequest.newBuilder(wellKnownUrl).build();
@@ -26,10 +31,25 @@ public class Utils {
 		}
 	}
 
-	static URI extractTokenUrlFromWellKnownData(String jsonData) {
+	public static URI extractTokenUrlFromWellKnownData(String jsonData) {
 		JsonObject dataAsObject = JsonParser.parseString(jsonData).getAsJsonObject();
 		String tokenUrl = dataAsObject.getAsJsonPrimitive("token_endpoint").getAsString();
 		return URI.create(tokenUrl);
 	}
 
+	public static String formMapToxWwwFormUrlencoded(Map<String, String> form) {
+		StringJoiner x_www_form_urlencoded = new StringJoiner("&");
+		form.forEach((key, value) -> x_www_form_urlencoded.add(key + "=" + value));
+		return x_www_form_urlencoded.toString();
+	}
+
+	public static HttpRequest formToHttpRequest(URI uri, Map<String, String> form) {
+		String body = formMapToxWwwFormUrlencoded(form);
+
+		return HttpRequest.newBuilder()
+				.POST(HttpRequest.BodyPublishers.ofString(body))
+				.uri(uri)
+				.header("Content-Type", "application/x-www-form-urlencoded")
+				.build();
+	}
 }
--- authentication/src/test/java/nl/esciencecenter/rsd/authentication/UtilsTest.java
+++ authentication/src/test/java/nl/esciencecenter/rsd/authentication/UtilsTest.java
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2022 - 2023 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2022 - 2023 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2022 - 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2022 - 2024 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 class UtilsTest {
 
@@ -76,4 +81,37 @@ class UtilsTest {
 		Assertions.assertThrows(ClassCastException.class, () -> Utils.extractTokenUrlFromWellKnownData(data));
 	}
 
+	@Test
+	void givenFormMap_whenConvertingToxWwwFormUrlencoded_thenCorrectResultReturned() {
+		Map<String, String> emptyForm = Map.of();
+		Assertions.assertEquals("", Utils.formMapToxWwwFormUrlencoded(emptyForm));
+
+		Map<String, String> singleEntryForm = Map.of("abc", "def");
+		Assertions.assertEquals("abc=def", Utils.formMapToxWwwFormUrlencoded(singleEntryForm));
+
+		// using a LinkedHashMap to ensure iteration order is insertion order
+		Map<String, String> multiEntryForm = new LinkedHashMap<>();
+		multiEntryForm.put("abc", "def");
+		multiEntryForm.put("123", "456");
+		Assertions.assertEquals("abc=def&123=456", Utils.formMapToxWwwFormUrlencoded(multiEntryForm));
+		multiEntryForm.put("key", "value");
+		Assertions.assertEquals("abc=def&123=456&key=value", Utils.formMapToxWwwFormUrlencoded(multiEntryForm));
+	}
+
+	@Test
+	void givenUriAndForm_whenConvertingToHttpRequest_thenCorrectResultReturned() {
+		URI uri = URI.create("https://www.example.com");
+		Map<String, String> form = Map.of("abc", "defgh");
+
+		HttpRequest request = Utils.formToHttpRequest(uri, form);
+
+		Assertions.assertEquals(uri, request.uri());
+		Assertions.assertEquals("POST", request.method());
+		HttpHeaders headers = request.headers();
+		List<String> contentTypeHeader = headers.allValues("Content-Type");
+		Assertions.assertEquals(1, contentTypeHeader.size());
+		Assertions.assertEquals("application/x-www-form-urlencoded", contentTypeHeader.getFirst());
+		int contentLength = Utils.formMapToxWwwFormUrlencoded(form).length();
+		Assertions.assertEquals(contentLength, request.bodyPublisher().orElseThrow().contentLength());
+	}
 }
