f16f57eb0741d6247609d759248cf1ad80ea1c59
Ewan Cahen <e.cahen@esciencecenter.nl>
2024-09-04 15:47:04 +0200

    nassa scraper

---
+++ scrapers/src/main/java/nl/esciencecenter/rsd/scraper/InvalidInputException.java
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2024 Netherlands eScience Center
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package nl.esciencecenter.rsd.scraper;
+
+public class InvalidInputException extends Exception {
+
+	public InvalidInputException(String message) {
+		super(message);
+	}
+}
