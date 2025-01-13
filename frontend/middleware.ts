7a8bf754325872b2ff2be3fd4497dad1a921c13b
Ewan Cahen <e.cahen@esciencecenter.nl>
2025-01-13 17:11:02 +0100

    fix: root of profile page now redirects to software

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2024 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2024 - 2025 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
+// SPDX-FileCopyrightText: 2024 - 2025 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,9 +7,15 @@ import {NextResponse} from 'next/server'
 import type {NextRequest} from 'next/server'
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(request.url + '/software')
+  if (request.nextUrl.pathname.startsWith('/communities')) {
+    return NextResponse.redirect(request.url + '/software')
+  }
+
+  if (request.nextUrl.pathname.startsWith('/profile')) {
+    return NextResponse.redirect(request.url + '/software')
+  }
 }
 
 export const config = {
-  matcher: '/communities/:path/',
+  matcher: ['/communities/:path/', '/profile/:orcid/']
 }
