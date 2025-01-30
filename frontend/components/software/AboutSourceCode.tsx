d3f6b023c5fadde5dd1f24118040de09adacc689
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-01-30 15:58:36 +0100

    fix: same logo size (height) for sourcecode and package managers

--- frontend/components/software/AboutSourceCode.tsx
+++ frontend/components/software/AboutSourceCode.tsx
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2025 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2025 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
 import GitHubIcon from '@mui/icons-material/GitHub'
 import FolderOpenIcon from '@mui/icons-material/FolderOpen'
 import {CodePlatform} from '~/types/SoftwareTypes'
-import GitlabIcon from '../../assets/logos/gitlab-icon-rgb.svg'
+import GitlabIcon from '~/assets/logos/gitlab-icon-rgb.svg'
 
 
 export default function AboutSourceCode({repository,platform}: { repository: string | null, platform: CodePlatform}) {
@@ -18,24 +20,33 @@ export default function AboutSourceCode({repository,platform}: { repository: str
     switch (platform) {
       case 'github':
         return (
-          <a key={repository} href={repository ?? ''} title="Github repository" target="_blank" rel="noreferrer">
+          <a key={repository} href={repository ?? ''}
+            title="Github repository" target="_blank" rel="noreferrer"
+            className="hover:text-base-content"
+          >
             <GitHubIcon sx={{
-              width: '3rem',
-              height: '3rem'
+              width: '3.25rem',
+              height: '3.25rem'
             }} />
           </a>
         )
       case 'gitlab':
         return (
-          <a key={repository} href={repository ?? ''} title="Gitlab repository" target="_blank" rel="noreferrer">
+          <a key={repository} href={repository ?? ''}
+            title="Gitlab repository" target="_blank" rel="noreferrer"
+            className="hover:text-base-content"
+          >
             <GitlabIcon
-              className="w-[3rem]"
+              className="w-[3rem] h-[3rem]"
             />
           </a>
         )
       default:
         return (
-          <a key={repository} href={repository ?? ''} title="Repository" target="_blank" rel="noreferrer">
+          <a key={repository} href={repository ?? ''}
+            title="Repository" target="_blank" rel="noreferrer"
+            className="hover:text-base-content"
+          >
             <FolderOpenIcon sx={{
               width: '3rem',
               height: '3rem',
