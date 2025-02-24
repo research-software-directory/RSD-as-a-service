566263aa848044d0e9ee7a4d2f4c8362f63a13f1
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-02-24 14:49:39 +0100

    WIP: filter lint for categories

--- frontend/components/layout/ContentLoader.tsx
+++ frontend/components/layout/ContentLoader.tsx
@@ -1,10 +1,12 @@
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 dv4all
+// SPDX-FileCopyrightText: 2025 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2025 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import ContentInTheMiddle from './ContentInTheMiddle'
 import CircularProgress from '@mui/material/CircularProgress'
+import ContentInTheMiddle from './ContentInTheMiddle'
 
 export default function ContentLoader() {
   return (
--- frontend/components/layout/TagChipFilter.tsx
+++ frontend/components/layout/TagChipFilter.tsx
@@ -5,23 +5,43 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import Link from 'next/link'
 import Chip from '@mui/material/Chip'
 import SearchIcon from '@mui/icons-material/Search'
-import Link from 'next/link'
+import CircularProgress from '@mui/material/CircularProgress'
 
 type TagChipFilterProps=Readonly<{
   label: string,
   url?:string ,
   title?: string
   capitalize?: boolean
+  loading?: boolean
 }>
 
 export default function TagChipFilter({
   url, label,
-  title, capitalize=true
+  title, capitalize=true,
+  loading
 }:TagChipFilterProps){
   // if no label no chip
   if (!label) return null
+
+  if (loading) return (
+    <Chip
+      title={title ? title : label}
+      label={label}
+      icon={<div><CircularProgress size={16} /></div>}
+      sx={{
+        maxWidth: '19rem',
+        borderRadius: '0.125rem',
+        textTransform: capitalize ? 'capitalize' : 'none',
+        '& .MuiChip-icon': {
+          order: 1,
+          margin:'0.5rem',
+        }
+      }}
+    />
+  )
 
   // simple chip without link
   if (!url) return (
