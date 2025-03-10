9e946079bd22269a0d77bceaf08747772af2c9ed
paulastock <paula.stock@gfz.de>
2025-03-10 13:46:21 +0100

    Revert erroneous header changes in PackageManagerServices

--- frontend/components/software/edit/services/PackageManagerServices.tsx
+++ frontend/components/software/edit/services/PackageManagerServices.tsx
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2023 - 2024 Dusan Mijatovic (Netherlands eScience Center)
 // SPDX-FileCopyrightText: 2023 - 2024 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2024 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
-// SPDX-FileCopyrightText: 2025 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
-// SPDX-FileCopyrightText: 2025 Paula Stock (GFZ) <paula.stock@gfz.de>
 //
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: EUPL-1.2
