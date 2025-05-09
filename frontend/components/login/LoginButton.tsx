49f147a89e1dee0eeae7615aa1d2933bcae5b80a
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-05-09 14:28:02 +0200

    fix: clickable mobile menu button fix: hide login button when no valid providers

--- frontend/components/login/LoginButton.tsx
+++ frontend/components/login/LoginButton.tsx
@@ -1,12 +1,12 @@
 // SPDX-FileCopyrightText: 2022 - 2023 Christian Meeßen (GFZ) <christian.meessen@gfz-potsdam.de>
 // SPDX-FileCopyrightText: 2022 - 2023 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
-// SPDX-FileCopyrightText: 2022 - 2024 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2022 - 2025 Netherlands eScience Center
 // SPDX-FileCopyrightText: 2022 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
 // SPDX-FileCopyrightText: 2022 Jesús García Gonzalez (Netherlands eScience Center) <j.g.gonzalez@esciencecenter.nl>
 // SPDX-FileCopyrightText: 2022 Matthias Rüster (GFZ) <matthias.ruester@gfz-potsdam.de>
 // SPDX-FileCopyrightText: 2022 dv4all
-// SPDX-FileCopyrightText: 2023 - 2024 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2023 - 2025 Dusan Mijatovic (Netherlands eScience Center)
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -41,7 +41,7 @@ export default function LoginButton() {
 
   // when there are multiple providers
   // we show modal with the list of login options
-  if (providers && providers.length > 1) {
+  if (providers?.length > 1) {
     return (
       <div className="whitespace-nowrap ml-2">
         <button
@@ -59,15 +59,19 @@ export default function LoginButton() {
     )
   }
 
-  // If there is only 1 provider we
-  // link redirect directly to Sign in button
-  return (
-    <Link
-      href={providers[0]?.redirectUrl ?? ''}
-      className="whitespace-nowrap ml-2" tabIndex={0}
-      passHref
-    >
-      Sign in
-    </Link>
-  )
+  if (providers?.length===1){
+    // if there is only 1 provider we link the redirect directly to Sign in button
+    return (
+      <Link
+        href={providers[0]?.redirectUrl ?? ''}
+        className="whitespace-nowrap ml-2" tabIndex={0}
+        passHref
+      >
+        Sign in
+      </Link>
+    )
+  }
+
+  // if no providers we do not shown login button
+  return null
 }
