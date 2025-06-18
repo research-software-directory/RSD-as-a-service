d2800652e564ca742d5f89c239ea0d782c576193
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-06-18 10:47:09 +0100

    Filter software, not projects.

--- frontend/components/home/imperial/KeywordBox.tsx
+++ frontend/components/home/imperial/KeywordBox.tsx
@@ -5,14 +5,14 @@
 
 import Link from 'next/link'
 
-import {ssrProjectsUrl} from '~/utils/postgrestUrl'
+import {ssrSoftwareUrl} from '~/utils/postgrestUrl'
 
 type KeywordBoxProps = {
     label: string
 }
 
 export default function KeywordBox({label}: KeywordBoxProps) {
-  const url = ssrProjectsUrl({keywords: [label]})
+  const url = ssrSoftwareUrl({keywords: [label]})
   return (
     <div className="bg-secondary text-primary-content p-2 text-center rounded-md">
       <div className="text-lg">
