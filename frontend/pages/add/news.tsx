ca9cd5af8cd14d2dde0ae7fe2209a2ae26b3b05e
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-07-01 17:14:42 +0200

    fix: disable add route when module is not present in settings.json

--- frontend/pages/add/news.tsx
+++ frontend/pages/add/news.tsx
@@ -1,9 +1,10 @@
-// SPDX-FileCopyrightText: 2024 Dusan Mijatovic (Netherlands eScience Center)
-// SPDX-FileCopyrightText: 2024 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2024 - 2025 Dusan Mijatovic (Netherlands eScience Center)
+// SPDX-FileCopyrightText: 2024 - 2025 Netherlands eScience Center
 //
 // SPDX-License-Identifier: Apache-2.0
 
 import RsdAdminContent from '~/auth/RsdAdminContent'
+import {getRsdModules} from '~/config/getSettingsServerSide'
 import AppFooter from '~/components/AppFooter'
 import AppHeader from '~/components/AppHeader'
 import PageContainer from '~/components/layout/PageContainer'
@@ -21,4 +22,22 @@ export default function AddNews() {
       <AppFooter />
     </>
   )
+}
+
+// fetching data server side
+// see documentation https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+export async function getServerSideProps() {
+  // check module is active
+  const modules = await getRsdModules()
+  // do not show software overview if module is not enabled
+  if (modules?.includes('news')===false){
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props:{
+      modules
+    }
+  }
 }
