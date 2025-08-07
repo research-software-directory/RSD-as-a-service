2550e9b6b89f50531951e538bd8f62f0646e7415
Ewan Cahen <e.cahen@esciencecenter.nl>
2025-08-07 15:12:42 +0200

    refactor: extract extra method from long main method for citation scraper

--- scrapers/src/main/java/nl/esciencecenter/rsd/scraper/doi/MainCitations.java
+++ scrapers/src/main/java/nl/esciencecenter/rsd/scraper/doi/MainCitations.java
@@ -28,87 +28,89 @@ public class MainCitations {
 		long start = System.currentTimeMillis();
 
 		try {
-			// Connect to the database to retrieve the reference papers to scrape
-
-			String backendUrl = Config.backendBaseUrl();
-			PostgrestCitationRepository localCitationRepository = new PostgrestCitationRepository(backendUrl);
-
-			Collection<CitationData> referencePapersToScrape = localCitationRepository.leastRecentlyScrapedCitations(
-				Config.maxCitationSourcesToScrape()
-			);
-
-			localCitationRepository.updateScrapedAtTime(
-				referencePapersToScrape.stream().map(CitationData::id).toList(),
-				Instant.now()
-			);
-
-			OpenAlexConnector openAlexConnector = new OpenAlexConnector();
-			PostgrestMentionRepository localMentionRepository = new PostgrestMentionRepository(backendUrl);
-			String email = Config.crossrefContactEmail().orElse(null);
-			Instant now = Instant.now();
-
-			for (CitationData citationData : referencePapersToScrape) {
-				long t1 = System.currentTimeMillis();
-
-				LOGGER.info("Scraping for DOI {}, OpenAlex ID {}", citationData.doi(), citationData.openalexId());
-
-				Collection<ExternalMentionRecord> citingMentions = openAlexConnector.citations(
-					citationData.openalexId(),
-					citationData.doi(),
-					email,
-					citationData.id()
-				);
-				// we don't update mentions that have a DOI in the database with OpenAlex data, as they can already be
-				// scraped through Crossref or DataCite
-
-				long t2 = System.currentTimeMillis();
-
-				citingMentions.removeIf(
-					mention -> mention.doi() != null && citationData.knownDois().contains(mention.doi())
-				);
-				Collection<RsdMentionIds> savedIds = new ArrayList<>(citingMentions.size());
-				for (ExternalMentionRecord citingMention : citingMentions) {
-					try {
-						RsdMentionIds ids = localMentionRepository.createOrUpdateMentionWithOpenalexId(
-							citingMention,
-							now
-						);
-						savedIds.add(ids);
-					} catch (Exception e) {
-						LOGGER.error("Unable to save exception with OpenAlex ID {}", citingMention.openalexId());
-						Utils.saveExceptionInDatabase("Citation scraper", "mention", null, e);
-					}
-				}
-
-				Collection<UUID> citingMentionIds = new ArrayList<>();
-				for (RsdMentionIds ids : savedIds) {
-					citingMentionIds.add(ids.id());
-				}
-
-				long t3 = System.currentTimeMillis();
-
-				localCitationRepository.saveCitations(backendUrl, citationData.id(), citingMentionIds);
-
-				long t4 = System.currentTimeMillis();
-
-				LOGGER.info(
-					"Scraping for {} done. OpenAlex: {} ms. Saving mentions {} ms. Saving citations {} ms. Total {} ms.",
-					citationData.doi(),
-					(t2 - t1),
-					(t3 - t2),
-					(t4 - t3),
-					(t4 - t1)
-				);
-			}
-		} catch (IOException | InterruptedException e) {
+			scrapeCitations();
+		} catch (RuntimeException | IOException e) {
+			LOGGER.error("Exception while scraping citations", e);
 			Utils.saveExceptionInDatabase("Citation scraper", null, null, e);
-
-			if (e instanceof InterruptedException) {
-				Thread.currentThread().interrupt();
-			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			LOGGER.warn("Got interrupted scraping citations, exiting");
+			return;
 		}
 
 		long time = System.currentTimeMillis() - start;
 		LOGGER.info("Done scraping citations ({} ms.)", time);
+	}
+
+	private static void scrapeCitations() throws IOException, InterruptedException {
+		// Connect to the database to retrieve the reference papers to scrape
+		String backendUrl = Config.backendBaseUrl();
+		PostgrestCitationRepository localCitationRepository = new PostgrestCitationRepository(backendUrl);
+
+		Collection<CitationData> referencePapersToScrape = localCitationRepository.leastRecentlyScrapedCitations(
+			Config.maxCitationSourcesToScrape()
+		);
+
+		// already update the citations_scraped_at fields to prevent them being scraped by a second process if this one takes too long
+		localCitationRepository.updateScrapedAtTime(
+			referencePapersToScrape.stream().map(CitationData::id).toList(),
+			Instant.now()
+		);
+
+		OpenAlexConnector openAlexConnector = new OpenAlexConnector();
+		PostgrestMentionRepository localMentionRepository = new PostgrestMentionRepository(backendUrl);
+		String email = Config.crossrefContactEmail().orElse(null);
+		Instant now = Instant.now();
+
+		for (CitationData citationData : referencePapersToScrape) {
+			long t1 = System.currentTimeMillis();
+
+			LOGGER.info("Scraping for DOI {}, OpenAlex ID {}", citationData.doi(), citationData.openalexId());
+
+			Collection<ExternalMentionRecord> citingMentions = openAlexConnector.citations(
+				citationData.openalexId(),
+				citationData.doi(),
+				email,
+				citationData.id()
+			);
+			// we don't update mentions that have a DOI in the database with OpenAlex data, as they can already be
+			// scraped through Crossref or DataCite
+
+			long t2 = System.currentTimeMillis();
+
+			citingMentions.removeIf(
+				mention -> mention.doi() != null && citationData.knownDois().contains(mention.doi())
+			);
+			Collection<RsdMentionIds> savedIds = new ArrayList<>(citingMentions.size());
+			for (ExternalMentionRecord citingMention : citingMentions) {
+				try {
+					RsdMentionIds ids = localMentionRepository.createOrUpdateMentionWithOpenalexId(citingMention, now);
+					savedIds.add(ids);
+				} catch (Exception e) {
+					LOGGER.error("Unable to save exception with OpenAlex ID {}", citingMention.openalexId());
+					Utils.saveExceptionInDatabase("Citation scraper", "mention", null, e);
+				}
+			}
+
+			Collection<UUID> citingMentionIds = new ArrayList<>();
+			for (RsdMentionIds ids : savedIds) {
+				citingMentionIds.add(ids.id());
+			}
+
+			long t3 = System.currentTimeMillis();
+
+			localCitationRepository.saveCitations(backendUrl, citationData.id(), citingMentionIds);
+
+			long t4 = System.currentTimeMillis();
+
+			LOGGER.info(
+				"Scraping for {} done. OpenAlex: {} ms. Saving mentions {} ms. Saving citations {} ms. Total {} ms.",
+				citationData.doi(),
+				(t2 - t1),
+				(t3 - t2),
+				(t4 - t3),
+				(t4 - t1)
+			);
+		}
 	}
 }
