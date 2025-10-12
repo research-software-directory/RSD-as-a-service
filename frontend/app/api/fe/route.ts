7d8e9d2dee50bc4481142ea5d6ae76c294aedeb4
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-10-12 21:37:28 +0200

    feat: show rsd version on fe/api route

--- frontend/app/api/fe/route.ts
+++ frontend/app/api/fe/route.ts
@@ -8,5 +8,10 @@ import app from '../../../package.json'
  * GET api/fe basic info
  */
 export async function GET() {
-  return Response.json({module: 'frontend/api', version: app?.version, status:'live'})
+  return Response.json({
+    module: 'frontend/api',
+    rsd_version: process.env.RSD_VERSION || 'MISSING',
+    package_version: app?.version,
+    status:'live'
+  })
 }
