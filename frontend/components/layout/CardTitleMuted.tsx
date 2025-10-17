6697a720bb2e52d93751f22f941d26aedff49de8
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2025-10-17 14:10:48 +0200

    fix: Use fixed title placoholder for research domain and country on the cards

--- frontend/components/layout/CardTitleMuted.tsx
+++ frontend/components/layout/CardTitleMuted.tsx
@@ -4,15 +4,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 type CardTitleMutedProps=Readonly<{
-  title: string
-  label: string
+  title?: string
+  label: string | null
 }>
 
 export default function CardTitleMuted({title,label}:CardTitleMutedProps) {
   return (
     <div
-      title={title}
-      className="line-clamp-1 text-sm text-base-content-secondary font-medium tracking-widest uppercase mb-2"
+      title={title ?? label ?? ''}
+      className="h-[1.25rem] line-clamp-1 text-sm text-base-content-secondary font-medium tracking-widest uppercase mb-2"
     >
       {label}
     </div>
