032a01685234177ad6216c57967686823605eba6
Ewan Cahen <e.cahen@esciencecenter.nl>
2026-04-01 16:52:42 +0200

    fix: fix spacing between news images

--- frontend/components/layout/ImageWithPlaceholder.tsx
+++ frontend/components/layout/ImageWithPlaceholder.tsx
@@ -1,7 +1,8 @@
 // SPDX-FileCopyrightText: 2022 - 2023 Dusan Mijatovic (dv4all)
 // SPDX-FileCopyrightText: 2022 - 2023 dv4all
 // SPDX-FileCopyrightText: 2023 - 2025 Dusan Mijatovic (Netherlands eScience Center)
-// SPDX-FileCopyrightText: 2023 - 2025 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2023 - 2026 Netherlands eScience Center
+// SPDX-FileCopyrightText: 2026 Ewan Cahen (Netherlands eScience Center) <e.cahen@esciencecenter.nl>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -56,9 +57,6 @@ export default function ImageWithPlaceholder({
       title={placeholder ?? alt}
       role="img"
       src={src}
-      // lighthouse audit requires explicit width and height
-      height="100%"
-      width="100%"
       style={{
         objectFit: bgSize,
         objectPosition: bgPosition
